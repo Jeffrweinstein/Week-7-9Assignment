906718ca9c8993a080710905a646c7b4e59eecce
Jeffrweinstein <jeffrweinstein@aol.com>
2023-02-18 22:06:17 -0800

    new commit

---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,15 @@
+package projects;
+
+import java.sql.Connection;
+
+import projects.dao.DbConnection;
+
+@SuppressWarnings("unused")
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DbConnection.getConnection();
+	}
+
+}
